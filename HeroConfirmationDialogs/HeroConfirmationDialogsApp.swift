1f3766cc8f56d15ec72aa0539fb4794241a75d49
Vitalii Kizlov <vitalii kizlov>
2025-11-24 12:59:47 +0100

    Initial commit

---
+++ HeroConfirmationDialogs/HeroConfirmationDialogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroConfirmationDialogsApp.swift
+//  HeroConfirmationDialogs
+//
+//  Created by Vitalii Kizlov on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct HeroConfirmationDialogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
